c8c65fa6b5c2fcfc7cb371c4b0d70008742a585e
Stephen  Chacha <48447675+stevechacha@users.noreply.github.com>
2024-08-16 14:11:00 +0300

    chacha-test

---
+++ shared/src/commonMain/kotlin/tz/co/asoft/interview/math/VectorImpl.kt
@@ -0,0 +1,21 @@
+package tz.co.asoft.interview
+
+import tz.co.asoft.interview.math.Vector
+
+class VectorImpl(override val x: Double, override val y: Double, override val magnitude: Double): Vector {
+    override fun plus(other: Vector): Vector {
+        TODO("Not yet implemented")
+    }
+
+    override fun minus(other: Vector): Vector {
+        TODO("Not yet implemented")
+    }
+
+    override fun unaryMinus(): Vector {
+        TODO("Not yet implemented")
+    }
+
+    override fun dot(other: Vector): Double {
+        TODO("Not yet implemented")
+    }
+}
